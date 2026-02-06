c0e4ac8f0e1316ad44929240e7217255f616f93a
kyrzixo <kyrzixo@gmail.com>
2026-02-05 20:25:26 -0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,17 +1,20 @@
 export default {
   async fetch(request) {
     const workerBase = "https://api.kyrzixo.workers.dev";
-
     const url = new URL(request.url);
 
     const targetUrl = workerBase + url.pathname + url.search;
 
-    const response = await fetch(targetUrl, {
+    // Create a new request to avoid body stream errors
+    const proxyRequest = new Request(targetUrl, {
       method: request.method,
       headers: request.headers,
-      body: request.body
+      body: request.method === "GET" || request.method === "HEAD"
+        ? null
+        : await request.clone().arrayBuffer(),
+      redirect: "manual"
     });
 
-    return response;
+    return fetch(proxyRequest);
   }
 };
